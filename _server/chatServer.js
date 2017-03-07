dc512c0f0a24242b52370e3feac138435ed47787
logicalThinker91 <shanelarson1991@gmail.com>
2017-03-07 16:02:05 -0600

    Added Files

---
+++ _server/chatServer.js
@@ -0,0 +1,26 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + "/clientFiles"));
+
+var server = require('http').createServer(app);
+
+var io = require('socket.io')(server);
+
+io.on('connection', function (client) {
+
+    client.join("globalChat");
+
+    client.on("shareMessage", function (data) {
+
+        io.sockets.in("globalChat").emit("recieveMessage", {
+            ip: client.request.connection.remoteAddress.replace("::ffff:",""),
+            message: data.text
+        });
+
+    });
+
+});
+
+server.listen(80);
---
+++ src/jsx/index.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MessageBox extends React.Component {
+    componentDidUpdate() {
+        this.refs.messageBox.scrollTop = this.refs.messageBox.scrollHeight;
+    }
+
+    render() {
+        const messages = this.props.messages.slice();
+        const messagesToDisplay = messages.map((message, index) => {
+            return (
+                <li key={index}>
+                    <span className="messageIp">[{message.ip}] :: </span>{message.text}
+                </li>
+            );
+        });
+        return <div className="messageBox" ref="messageBox">
+            <ul>{messagesToDisplay}</ul>
+        </div>;
+    }
+}
+
+class MessageBar extends React.Component {
+    render() {
+        return <input className="messageBar" onKeyUp={
+            (e) => {
+                if (e.keyCode == 13) {
+                    this.props.sendMessage(e.target.value);
+                    e.target.value = "";
+                }
+            }
+        }/>;
+    }
+}
+
+class ChatApp extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            socket: null,
+            messages: []
+        };
+    }
+
+    componentDidMount() {
+        let socket = io('http://54.85.5.71');
+        let messages = this.state.messages;
+        socket.on('connect', function () {
+            // alert("online");
+        });
+        socket.on('recieveMessage', function (data) {
+            let socket = this.state.socket;
+            let messages = this.state.messages.slice();
+            this.setState({
+                socket: socket,
+                messages: messages.concat({
+                    ip: data.ip,
+                    text: data.message
+                })
+            });
+        }.bind(this));
+        socket.on('disconnect', function () {
+            // alert("offline");
+        });
+        this.setState({
+            socket: socket,
+            messages: messages
+        });
+    }
+
+    sendMessage(message) {
+        let socket = this.state.socket;
+        socket.emit("shareMessage", {
+            text: message
+        });
+    }
+
+    render() {
+        const messages = this.state.messages;
+        return (
+            <div>
+                <h2>ReactJS & Node.js w/ Socket.io Chat Example</h2>
+                <MessageBox messages={messages}/>
+                <MessageBar sendMessage={this.sendMessage.bind(this)}/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<ChatApp/>, document.querySelector("#chatApp"));
